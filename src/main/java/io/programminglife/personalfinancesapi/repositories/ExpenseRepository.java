efe625c84eec3d430ad385a3c5a2dbd5c56e3a84
Andrei Visan <andrei.m.visan@gmail.com>
2021-06-08 18:26:47 +0200

    Finalized repositories

--- src/main/java/io/programminglife/personalfinancesapi/repositories/ExpenseRepository.java
+++ src/main/java/io/programminglife/personalfinancesapi/repositories/ExpenseRepository.java
@@ -1,9 +1,22 @@
 package io.programminglife.personalfinancesapi.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import io.programminglife.personalfinancesapi.entities.Expense;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+    @Query(value = "SELECT * FROM expense WHERE label = :label", nativeQuery = true)
+    List<Expense> findExpensesByLabelEquals(@Param("label") String label);
+
+    @Query(value = "SELECT * FROM expense WHERE category_id = :categoryId", nativeQuery = true)
+    List<Expense> findExpensesByCategoryEquals(@Param("categoryId") Long categoryId);
+
+    @Query(value = "SELECT * FROM expense WHERE payment_system_id = :paymentSystemId", nativeQuery = true)
+    List<Expense> findExpensesByPaymentSystemEquals(@Param("paymentSystemId") Long paymentSystemId);
 
 }
