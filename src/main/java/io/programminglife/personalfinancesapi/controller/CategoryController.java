6267e1902d052227ed7a19206a93d17436a12caa
Andrei Visan <andrei.m.visan@gmail.com>
2021-06-09 18:25:22 +0200

    REST API - Category API - Done

--- src/main/java/io/programminglife/personalfinancesapi/controller/CategoryController.java
+++ src/main/java/io/programminglife/personalfinancesapi/controller/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,11 @@ public class CategoryController {
         } catch (MyFinancesException e) {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable(value = "id") Long id) {
+        categoryService.deleteCategory(id);
     }
 
 }
