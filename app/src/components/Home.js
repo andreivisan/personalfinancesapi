552143829b29627ebcf22abb13a5335fb9e23668
Andrei Visan <andrei.m.visan@gmail.com>
2021-06-16 16:17:21 +0200

    Implemented React Router for Side Bar

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,100 +1,26 @@
-import axios from 'axios';
 import ReactDOM from 'react-dom';
-import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { Component } from 'react';
 
-import EditableTable from './EditableTable';
+import SideBar from './SideBar';
+import UploadExpenses from './UploadExpenses';
 
 class Home extends Component {
-    state = { 
-        selectedFile: null,
-        csvEntities: null
+    constructor(props) {
+        super(props);
+        this.state = {  }
     }
-
-    toggleSideNav() {
-        const sidebar = document.querySelector(".sidebar");
-        sidebar.classList.toggle("-translate-x-full");
-    }
-
-    onFileChange = event => {
-        this.setState({selectedFile: event.target.files[0]});
-    }
-
-    onFileUpload = () => {  
-        const formData = new FormData(); 
-       
-        formData.append( 
-          "expensesCsv", 
-          this.state.selectedFile
-        ); 
-       
-        axios.post("api/v1/fileupload/", formData)
-             .then((response) => {
-                //  this.state.csvEntities = response.data;
-                ReactDOM.render(
-                    <EditableTable csvEntities={response.data} />,
-                    document.getElementById("editableTable")
-                );
-             });
-    }; 
 
     render() { 
-        return (
+        return ( 
             <div class="relative min-h-screen md:flex">
-                <div class="bg-gray-900 text-gray-100 flex justify-between md:hidden">                    
-                    <a href="#" class="block p-4 text-white font-bold">Better Dev</a>
+                <Router>
+                    <SideBar />
 
-                    <button class="mobile-menu-button p-4 focus:outline-none focus:bg-gray-700" onClick={this.toggleSideNav}>
-                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                    </button>
-                </div>
-                
-                <div class="sidebar bg-gray-900 md:bg-gray-900 text-blue-100 w-70 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out md:border-r-4 md:border-gray-600">
-                    <a href="#" class="text-white flex items-center space-x-2 px-4">
-                    <svg class="w-12 h-12" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path d="M17 12c-3.313 0-6 2.687-6 6s2.687 6 6 6 6-2.687 6-6-2.687-6-6-6zm.5 8.474v.526h-.5v-.499c-.518-.009-1.053-.132-1.5-.363l.228-.822c.478.186 1.114.383 1.612.27.574-.13.692-.721.057-1.005-.465-.217-1.889-.402-1.889-1.622 0-.681.52-1.292 1.492-1.425v-.534h.5v.509c.362.01.768.073 1.221.21l-.181.824c-.384-.135-.808-.257-1.222-.232-.744.043-.81.688-.29.958.856.402 1.972.7 1.972 1.773.001.858-.672 1.315-1.5 1.432zm1.624-10.179c1.132-.223 2.162-.626 2.876-1.197v.652c0 .499-.386.955-1.007 1.328-.581-.337-1.208-.6-1.869-.783zm-2.124-5.795c2.673 0 5-1.007 5-2.25s-2.327-2.25-5-2.25c-2.672 0-5 1.007-5 2.25s2.328 2.25 5 2.25zm.093-2.009c-.299-.09-1.214-.166-1.214-.675 0-.284.334-.537.958-.593v-.223h.321v.211c.234.005.494.03.784.09l-.116.342c-.221-.051-.467-.099-.708-.099l-.072.001c-.482.02-.521.287-.188.399.547.169 1.267.292 1.267.74 0 .357-.434.548-.967.596v.22h-.321v-.208c-.328-.003-.676-.056-.962-.152l.147-.343c.244.063.552.126.828.126l.208-.014c.369-.053.443-.3.035-.418zm-11.093 13.009c1.445 0 2.775-.301 3.705-.768.311-.69.714-1.329 1.198-1.899-.451-1.043-2.539-1.833-4.903-1.833-2.672 0-5 1.007-5 2.25s2.328 2.25 5 2.25zm.093-2.009c-.299-.09-1.214-.166-1.214-.675 0-.284.335-.537.958-.593v-.223h.321v.211c.234.005.494.03.784.09l-.117.342c-.22-.051-.466-.099-.707-.099l-.072.001c-.482.02-.52.287-.188.399.547.169 1.267.292 1.267.74 0 .357-.434.548-.967.596v.22h-.321v-.208c-.329-.003-.676-.056-.962-.152l.147-.343c.244.063.552.126.828.126l.208-.014c.368-.053.443-.3.035-.418zm4.003 8.531c-.919.59-2.44.978-4.096.978-2.672 0-5-1.007-5-2.25v-.652c1.146.918 3.109 1.402 5 1.402 1.236 0 2.499-.211 3.549-.611.153.394.336.773.547 1.133zm-9.096-3.772v-.651c1.146.917 3.109 1.401 5 1.401 1.039 0 2.094-.151 3.028-.435.033.469.107.926.218 1.37-.888.347-2.024.565-3.246.565-2.672 0-5-1.007-5-2.25zm0-2.5v-.652c1.146.918 3.109 1.402 5 1.402 1.127 0 2.275-.176 3.266-.509-.128.493-.21 1.002-.241 1.526-.854.298-1.903.483-3.025.483-2.672 0-5-1.007-5-2.25zm11-11v-.652c1.146.918 3.109 1.402 5 1.402 1.892 0 3.854-.484 5-1.402v.652c0 1.243-2.327 2.25-5 2.25-2.672 0-5-1.007-5-2.25zm0 5v-.652c.713.571 1.744.974 2.876 1.197-.661.183-1.287.446-1.868.783-.622-.373-1.008-.829-1.008-1.328zm0-2.5v-.651c1.146.917 3.109 1.401 5 1.401 1.892 0 3.854-.484 5-1.401v.651c0 1.243-2.327 2.25-5 2.25-2.672 0-5-1.007-5-2.25z"/>
-                    </svg>
-                    <span class="text-2xl font-extrabold">MY FINANCES</span>
-                    </a>
-
-                    <nav>
-                    <a href="#" class="block py-2.5 px-4 rounded transition duration-200 hover:text-white">
-                        Upload Expenses
-                    </a>
-                    <a href="" class="block py-2.5 px-4 rounded transition duration-200 hover:text-white">
-                        Dashboard
-                    </a>
-                    </nav>
-                </div>
-
-                <div class="flex-1 p-10 text-2xl font-bold bg-gray-100">
-                    <div class="text-2xl">UPLOAD EXPENSES</div>
-
-                    <div class="mt-12 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                        <div class="space-y-1 text-center">
-                            <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 24 24" aria-hidden="true">
-                                {/* <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /> */}
-                                <path d="M6 12h10v1h-10v-1zm7.816-3h-7.816v1h9.047c-.45-.283-.863-.618-1.231-1zm5.184 1.975v2.569c0 4.106-6 2.456-6 2.456s1.518 6-2.638 6h-7.362v-20h9.5c.312-.749.763-1.424 1.316-2h-12.816v24h10.189c3.163 0 9.811-7.223 9.811-9.614v-3.886c-.623.26-1.297.421-2 .475zm-13-3.975h6.5c-.134-.32-.237-.656-.319-1h-6.181v1zm17-2.5c0 2.485-2.017 4.5-4.5 4.5s-4.5-2.015-4.5-4.5 2.017-4.5 4.5-4.5 4.5 2.015 4.5 4.5zm-2-.5h-2v-2h-1v2h-2v1h2v2h1v-2h2v-1z"/>
-                            </svg>
-                            <div class="flex text-sm text-gray-600">
-                                <label for="file-upload" class="mt-2 relative cursor-pointe rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                                    <span>Upload a file</span>
-                                    <input id="file-upload" name="file-upload" type="file" class="sr-only" onChange={this.onFileChange}/>
-                                </label>
-                                <p class="mt-2 pl-1">or drag and drop</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="px-4 py-3 text-right sm:px-1">
-                        <button class="inline-flex justify-center hover:bg-white text-sm font-medium py-2 px-4 border border-gray-400 rounded shadow" onClick={this.onFileUpload}>
-                            Upload
-                        </button>
-                    </div>
-
-                    <div id="editableTable" class="text-sm text-gray-900"></div>
-                </div>
+                    <Switch>
+                        <Route path="/uploadExpenses" component={UploadExpenses} />
+                    </Switch>
+                </Router>
             </div>
         );
     }
---
+++ app/src/components/UploadExpenses.js
@@ -0,0 +1,66 @@
+import axios from 'axios';
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+
+import EditableTable from './EditableTable';
+
+class UploadExpenses extends Component {
+    state = { 
+        selectedFile: null,
+        csvEntities: null
+    }
+
+    onFileChange = event => {
+        this.setState({selectedFile: event.target.files[0]});
+    }
+
+    onFileUpload = () => {  
+        const formData = new FormData(); 
+       
+        formData.append( 
+          "expensesCsv", 
+          this.state.selectedFile
+        ); 
+       
+        axios.post("api/v1/fileupload/", formData)
+             .then((response) => {
+                ReactDOM.render(
+                    <EditableTable csvEntities={response.data} />,
+                    document.getElementById("editableTable")
+                );
+             });
+    }; 
+
+    render() { 
+        return (
+            <div class="flex-1 p-10 text-2xl font-bold bg-gray-100">
+                <div class="text-2xl">UPLOAD EXPENSES</div>
+
+                <div class="mt-12 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                    <div class="space-y-1 text-center">
+                        <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 24 24" aria-hidden="true">
+                            <path d="M6 12h10v1h-10v-1zm7.816-3h-7.816v1h9.047c-.45-.283-.863-.618-1.231-1zm5.184 1.975v2.569c0 4.106-6 2.456-6 2.456s1.518 6-2.638 6h-7.362v-20h9.5c.312-.749.763-1.424 1.316-2h-12.816v24h10.189c3.163 0 9.811-7.223 9.811-9.614v-3.886c-.623.26-1.297.421-2 .475zm-13-3.975h6.5c-.134-.32-.237-.656-.319-1h-6.181v1zm17-2.5c0 2.485-2.017 4.5-4.5 4.5s-4.5-2.015-4.5-4.5 2.017-4.5 4.5-4.5 4.5 2.015 4.5 4.5zm-2-.5h-2v-2h-1v2h-2v1h2v2h1v-2h2v-1z"/>
+                        </svg>
+                        <div class="flex text-sm text-gray-600">
+                            <label class="mt-2 relative cursor-pointe rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                <span>Upload a file</span>
+                                <input id="file-upload" name="file-upload" type="file" class="sr-only" onChange={this.onFileChange}/>
+                            </label>
+                            <p class="mt-2 pl-1">or drag and drop</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="px-4 py-3 text-right sm:px-1">
+                    <button class="inline-flex justify-center hover:bg-white text-sm font-medium py-2 px-4 border border-gray-400 rounded shadow" onClick={this.onFileUpload}>
+                        Upload
+                    </button>
+                </div>
+
+                <div id="editableTable" class="text-sm text-gray-900"></div>
+            </div>
+        );
+    }
+}
+ 
+export default UploadExpenses;
